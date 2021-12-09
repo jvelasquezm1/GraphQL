46f2f6024b0db9345155ae3a435e958d0297a803
Juanes Velasquez <jvelasquez@unac.edu.co>
2021-12-09 09:28:00 +0100

    Set up environment

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+
+const typeDefs = gql`
+type Query {
+    sessions: [Session]
+}
+type Session {
+    id: ID!,
+    title: String!,
+    description:String,
+    startsAt:String,
+    endsAt:String,
+    room:String,
+    day:String,
+    format:String,
+    track:String,
+    level:String
+}`
+
+const server = new ApolloServer({ typeDefs });
+server.listen({ port: 4000 }).then(({ url }) => { console.log(`running at ${url}`) })
